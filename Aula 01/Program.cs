dac7a4d3ec3e90951f0de5a9a483c29961921386
Sabrinasilva335 <sabrina.squeiroz2@senacsp.edu.br>
2025-02-26 14:49:01 -0300

    Upload dos códigos da primeira aula

---
+++ Aula 01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 23;
+            string nomeCompleto = "Sabrina Silva Queiroz";
+
+            var variavelSemTipo = "Sabrina";
+
+            Console.WriteLine(variavelSemTipo.GetType());
+
+            Console.WriteLine("Digite o seu nome: ");
+
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Digite a sua idade: ");
+            string idadeUsuario = Console.ReadLine();
+
+
+
+
+            Console.WriteLine($"Meu nome é {nomeUsuario} e a minha idade  é {idade} ");
+        }
+    }
+}
